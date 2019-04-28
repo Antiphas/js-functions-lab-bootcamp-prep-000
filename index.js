3034844a4ad2c097baf5d4e23f8f52af8f7e9c42
Gufwat Antiphas <antiphas4christ888@gmail.com>
2019-04-28 03:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // write your code below!
+function happyHolidays(greeting){
+  return greeting
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name){
+  return `Happy Independence ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday){
+  return `It's ${days}, days until ${holiday}!`
+}
+happyHolidays('Happy holidays!')
+happyHolidaysTo('you')
+happyHolidayTo('Day','you')
+holidayCountdown(20,25)
